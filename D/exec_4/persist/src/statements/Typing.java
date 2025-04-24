f3eacba11c78806628ea28a0b2fc3e8a6726ac95
Daniel Rybarczyk <mikjodr@gmail.com>
2025-04-24 17:15:08 +0200

    Words game

--- D/exec_4/persist/src/statements/Typing.java
+++ D/exec_4/persist/src/statements/Typing.java
@@ -1,5 +1,8 @@
 package statements;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Typing {
 
     public static void main(String[] args) {
@@ -38,27 +41,42 @@ public class Typing {
             Napisz fragment kodu, który dla zadanego tekstu obliczy maksymalną ilość punktów do zdobycia.
          */
 
-        String data = "Ania;je;jabłko;Ma;rumiane;policzki;Paweł i Robert;zrywają;jabkła z jabłoni;" +
-                "Jedzą;dojrzałe, żółte gruszki;Piją zimną;krystaliczną wodę;Ania;zrywa;truskawki;Żeby;później;" +
-                "zrobić z nich dżem;Paweł i Robert;uwielbiają;dżem Ani;Gdy nadchodzi jesień;cała kuchnia;" +
-                "pachnie konfiturami;Ani Robert;otwiera;słoik z kompotem;z czereśni;Paweł;piecze;malinową;tartę";
 
-        double maxPoints;
-        {
-            maxPoints = -1;
-        }
-
+		
+		// {
+			// String currentWord  = "";
+			
+			// for (int i = 0; i < data.length(); i++) {
+				// char c = data.charAt(i);
+				
+				// if (c != ';') {
+					// maxPoints++;
+				// }
+			// }
+			
+			// if (!currentWord.isEmpty()) {
+				// maxPoints += currentWord.length();
+			// }
+			
+			// System.out.println("Maksymalna ilość pkt do zdobycia: " + maxPoints);
+		// }
+		
 
         /*
             Napisz fragment kodu, który dla zadanego tekstu odnajdzie ostatnie wystąpienie w nim symbolu
             średnika, a następnie wylosuje wartość liczbową od 0 do pozycji, na której występuje ten średnik.
          */
 
-        int position;
-        {
-            String separator = ";";
-            position = -1;
-        }
+        // int position;
+        // {
+            // String separator = ";";
+  
+			// int lastIndexOfSeparator = data.lastIndexOf(separator);
+			
+			// Random r = new Random();
+			// position = r.nextInt(lastIndexOfSeparator);
+			// System.out.println("Ostatni średnik na pozycji: " + lastIndexOfSeparator + " i losowa pozycja: " + position);
+        // }
 
 
         /*
@@ -66,10 +84,20 @@ public class Typing {
             oraz zapamięta napis występujący po tym średniku do następnego średnika lub końca tekstu.
          */
 
-        String text;
-        {
-            text = "";
-        }
+        // String text;
+        // {
+			
+			// String separator = ";";
+			// int indexOfSeparator = data.indexOf(separator, position);
+			// int indexOfNextSeparator = data.indexOf(separator, indexOfSeparator + 1);
+			
+			// if (indexOfNextSeparator == -1) {
+				// text = data.substring(indexOfSeparator + 1);
+			// } else {
+				// text = data.substring(indexOfSeparator + 1, indexOfNextSeparator);
+			// }
+        // }
+		// System.out.println(text);
 
 
         /*
@@ -77,11 +105,26 @@ public class Typing {
             zgodności drugiego napisu z pierwszym i zasad opisanych na początku klasy
          */
 
-        String userText = "";
-        double points;
-        {
-            points = -1;
-        }
+
+        // double points = 0;
+        // {
+			// Scanner scanner = new Scanner(System.in);
+			// String userText = scanner.nextLine();
+			
+			// for (int i = 0; i < text.length() && i < userText.length(); i++) {
+				// char letter = text.charAt(i);
+				
+				// char userLetter = userText.charAt(i);
+				
+				// if (letter == userLetter) {
+					// points++;
+				// } else if (Character.toLowerCase(letter) == Character.toLowerCase(userLetter)) {
+					// points = points + 0.5;
+				// }
+			// }
+			// System.out.println("Uzyskana liczba punktów: " + points);
+            
+        // }
 
 
         /*
@@ -90,10 +133,17 @@ public class Typing {
             które upłynęło od północy 1 stycznia 1970 roku.
          */
 
-        long startTime = -1;
-        {
-            long currentTime = -1;
-        }
+        // long startTime = System.currentTimeMillis();
+        // {
+			// while (true) {
+				// long currentTime = System.currentTimeMillis();
+				
+				// if ((currentTime - startTime) > 2_000) {
+					// System.out.println("Czas koniec. " + "Minęło już: " + (currentTime - startTime)/1000 + "sek.");
+					// break;
+				// }
+			// }
+        // }
 
 
         /*
@@ -102,5 +152,106 @@ public class Typing {
 
             Uzupełnij je o dodatkowe elementy, które pojawiły się w opisie zadania.
          */
+		 
+		String data = "Ania;je;jabłko;Ma;rumiane;policzki;Paweł i Robert;zrywają;jabkła z jabłoni;" +
+        "Jedzą;dojrzałe, żółte gruszki;Piją zimną;krystaliczną wodę;Ania;zrywa;truskawki;Żeby;później;" +
+        "zrobić z nich dżem;Paweł i Robert;uwielbiają;dżem Ani;Gdy nadchodzi jesień;cała kuchnia;" +
+        "pachnie konfiturami;Ani Robert;otwiera;słoik z kompotem;z czereśni;Paweł;piecze;malinową;tartę";
+
+        double maxPoints = 0;
+		double userPoints = 0;
+		 
+		long startTime = System.currentTimeMillis();
+		
+		// *** MAXYMALNA MOŻLIWA LICZBA PUNKTÓW ***
+        {
+			{
+				String currentWord  = "";	
+				for (int i = 0; i < data.length(); i++) {
+					char c = data.charAt(i);
+					if (c != ';') {
+						maxPoints++;
+					}
+				}
+					
+				if (!currentWord.isEmpty()) {
+					maxPoints += currentWord.length();
+				}
+				System.out.println("Maksymalna ilość pkt do zdobycia: " + maxPoints);
+			}
+			while (true) {
+				long currentTime = System.currentTimeMillis();
+				
+				
+			// Zakończ jeśli nie ma już słów
+				if (!data.contains(";")) {
+                System.out.println("Wszystkie słowa zostały wykorzystane.");
+                break;
+				}
+				
+				// *** OKREŚLENIE MIEJSCA OSTATNIEGO ';' I WYLOSOWANIE MIEJSCA W TEKŚCIE ***
+				int position;
+				{
+					Random r = new Random();
+					String separator = ";";
+					int lastIndexOfSeparator = data.lastIndexOf(separator);
+					position = r.nextInt(lastIndexOfSeparator);
+				}
+				
+				// *** WYŚWIETLENIE SŁOWA DO PRZEPISANIA (KTÓRE ZNAJDUJE SIĘ JAKO PIERWSZE PO WYLOSOWANYM W POPRZEDNIM PUNKCIE MIEJSCU) ***
+				String text;
+				
+					
+					String separator = ";";
+					int indexOfSeparator = data.indexOf(separator, position);
+					int indexOfNextSeparator = data.indexOf(separator, indexOfSeparator + 1);
+					
+					if (indexOfNextSeparator == -1) {
+						text = data.substring(indexOfSeparator + 1);
+					} else {
+						text = data.substring(indexOfSeparator + 1, indexOfNextSeparator);
+					}
+					
+				
+				
+				System.out.println(text);	
+				
+				// ZLICZENIE PUNKTÓW ZA WPISANE SŁÓWKO
+				double points;
+				{
+
+					Scanner scanner = new Scanner(System.in);
+					String userText = scanner.nextLine();
+					points = 0;
+					
+					for (int i = 0; i < text.length() && i < userText.length(); i++) {
+						char letter = text.charAt(i);
+						
+						char userLetter = userText.charAt(i);
+						
+						if (letter == userLetter) {
+							points++;
+						} else if (Character.toLowerCase(letter) == Character.toLowerCase(userLetter)) {
+							points += 0.5;
+						}
+					}
+				}
+				userPoints += points;
+				
+				// *** USUNIĘCIE UŻYTEGO SŁOWA Z "data" ***
+				if (indexOfNextSeparator == -1) {
+					data = data.substring(0, indexOfSeparator + 1);
+				} else {
+					data = data.substring(0, indexOfSeparator + 1) + data.substring(indexOfNextSeparator + 1);
+				}
+				
+				// *** ZATRZYMANIE PROGRAMU PO OKREŚLONYM CZASIE ***
+				if ((currentTime - startTime) > 60_000) {
+					System.out.println("Czas koniec. " + "Minęło już: " + (currentTime - startTime)/1000 + "sek.");
+					break;
+				}
+			}
+			System.out.println("Zdobyłeś: " + userPoints + "/" + maxPoints + " punktów");
+        }
     }
 }
