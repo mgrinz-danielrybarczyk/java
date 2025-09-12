4060e1329afbb07c290b48e3e9073e500c125261
Daniel Rybarczyk <mikjodr@gmail.com>
2025-09-12 22:34:58 +0200

    value returning methods

--- E/exec_2/main/src/returning/Write.java
+++ E/exec_2/main/src/returning/Write.java
@@ -1,10 +1,26 @@
 package returning;
 
+import java.util.Scanner;
+
 public class Write {
 
     public static void main(String[] args) {
 
         // Dostarcz poniżej po trzy różne przypadki użycia wskazanych metod
+		System.out.println(countNumbers(4,8));
+		System.out.println(countNumbers(2,6));
+		System.out.println(countNumbers(3,7));
+		
+		System.out.println(addValues(5,3,17));
+		System.out.println(addValues(5,3,18));
+		System.out.println(addValues(5,3,16));
+		
+		System.out.println(giveMeNumber("Cześć! Podaj mi liczbę z zakresu: ",3,16));
+		
+		
+		
+		
+		
     }
 
     /*
@@ -15,7 +31,43 @@ public class Write {
         Liczby powinny być rozdzielone od siebie znakiem przecinka. Po ostatniej liczbie nie powinno
         być przecinka.
      */
-
+	 public static String countNumbers(int firstNumber, int secondNumber) {
+		 String numbers = "";
+		 /*
+		 if (firstNumber < secondNumber) {
+			 for(int i = firstNumber; i <= secondNumber; i++) {
+				if (i != secondNumber) {
+				 numbers += firstNumber + ",";
+				 firstNumber += 1;
+				} else {
+				 numbers += firstNumber;
+				}		 
+			 }
+		 } else if (firstNumber > secondNumber) {
+			 for(int i = secondNumber; i <= firstNumber; i++) {
+				if (i != firstNumber) {
+				 numbers += secondNumber + ",";
+				 secondNumber += 1;
+				} else {
+				 numbers += secondNumber;
+				}		 
+			 }	 
+		} else {
+			numbers += firstNumber;
+		}
+		return numbers;
+	 }
+	 */
+		 int i = firstNumber;
+		 while (i <= secondNumber) {
+			 numbers += i;
+			 if (i != secondNumber) {
+				 numbers += ",";
+			 }
+			 i++;
+		 }
+		return numbers;
+}
 
     /*
         Tutaj dostarcz kod drugiej metody.
@@ -32,7 +84,16 @@ public class Write {
 
         Wynik: 5 + 8 + 11 + 14 + 17 + 20 = 75
      */
-
+	 
+		 public static int addValues(int startNumber, int add, int endNumber) {
+			 int result = startNumber;
+			 int sum = startNumber;
+			 while (result <= endNumber) {
+				 result += add;
+				 sum += result;
+			 }
+			 return sum;
+		 }
 
     /*
         Tutaj dostarcz kod trzeciej metody.
@@ -45,4 +106,19 @@ public class Write {
 
         Metoda powinna zwrócić poprawną wartość podaną przez użytkownika
      */
+	 public static int giveMeNumber(String text, int firstValue, int secondValue) {
+		Scanner scanner = new Scanner(System.in);
+		int userValue;
+		do {
+			System.out.println(text + firstValue + " - " + secondValue);
+			userValue = scanner.nextInt();
+		}
+		while (userValue < firstValue || userValue > secondValue);
+		System.out.print("Świetnie! Podałeś prawidłową wartość: ");
+		return userValue;
+	 }
+	 
+
+	 
+	 
 }
