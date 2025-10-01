36f856b3313671acf0a41ec6a0e54890431aa79e
Daniel Rybarczyk <mikjodr@gmail.com>
2025-10-01 20:50:17 +0200

    static variables

--- E/exec_3/main/src/statics/Donts.java
+++ E/exec_3/main/src/statics/Donts.java
@@ -2,39 +2,42 @@ package statics;
 
 public class Donts {
 
-	public static int sum;
-	public static int base;
+	public static int sum = 0;
+	public static int base = 2;
 
     public static void main(String[] args) {
-		sum = 0;
+
         System.out.println("Suma liczb 3 i 4 to: " + sumNumbers(3, 4));
-		sum = 0;
+
 		System.out.println("Suma liczb 5 i 6 to: " + sumNumbers(5, 6));
-		sum = 0;
+
         System.out.println("Suma liczb 10 i 12 to: " + sumNumbers(10, 12));
 		
-		base = 2;
-        System.out.println("Wynik 2^5 to: " + pow(5));
-		base = 3;
-        System.out.println("Wynik 3^4 to: " + pow(4));
-		base = 10;
-        System.out.println("Wynik 10^3 to: " + pow(3));
+
+        System.out.println("Wynik 2^5 to: " + pow(2, 5));
+
+        System.out.println("Wynik 3^4 to: " + pow(3, 4));
+
+        System.out.println("Wynik 10^3 to: " + pow(10, 3));
 
         printCurrentSum();
         printCurrentBase();
     }
 
     public static int sumNumbers(int a, int b) {
+		int sum = 0;
         sum += a;
         sum += b;
+		Donts.sum += sum;
         return sum;
     }
 
-    public static long pow(int pow) {
+    public static long pow(int base, int pow) {
         long result = 1L;
         for (int i = 1; i <= pow; i++) {
             result *= base;
         }
+		Donts.base = base;
         return result;
     }
 
