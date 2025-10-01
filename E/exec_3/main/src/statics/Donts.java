f0b872b6b9153f898af69b6fa11b6d59785a6706
Daniel Rybarczyk <mikjodr@gmail.com>
2025-10-01 18:38:59 +0200

    Bad use of static variables

--- E/exec_3/main/src/statics/Donts.java
+++ E/exec_3/main/src/statics/Donts.java
@@ -2,18 +2,23 @@ package statics;
 
 public class Donts {
 
-    public static int sum = 0;
-    public static int base = 2;
+	public static int sum;
+	public static int base;
 
     public static void main(String[] args) {
-
+		sum = 0;
         System.out.println("Suma liczb 3 i 4 to: " + sumNumbers(3, 4));
-//        System.out.println("Suma liczb 5 i 6 to: " + sumNumbers(5, 6));
-//        System.out.println("Suma liczb 10 i 12 to: " + sumNumbers(10, 12));
-
+		sum = 0;
+		System.out.println("Suma liczb 5 i 6 to: " + sumNumbers(5, 6));
+		sum = 0;
+        System.out.println("Suma liczb 10 i 12 to: " + sumNumbers(10, 12));
+		
+		base = 2;
         System.out.println("Wynik 2^5 to: " + pow(5));
-//        System.out.println("Wynik 3^4 to: " + pow(4));
-//        System.out.println("Wynik 10^3 to: " + pow(3));
+		base = 3;
+        System.out.println("Wynik 3^4 to: " + pow(4));
+		base = 10;
+        System.out.println("Wynik 10^3 to: " + pow(3));
 
         printCurrentSum();
         printCurrentBase();
